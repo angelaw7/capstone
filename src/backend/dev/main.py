4bbb983b3d26d1ee406c3bad8672a08b310368ab
Angela Wang <wangela472@gmail.com>
2024-11-25 16:14:40 -0500

    draft

--- src/backend/dev/main.py
+++ src/backend/dev/main.py
@@ -1,83 +1,92 @@
 from PIL import Image, ImageEnhance, ImageFilter
+import pandas as pd
 import pytesseract
 import numpy as np
-import re
+
+import utils
+
 
 pytesseract.pytesseract.tesseract_cmd = (
     r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 )
-
-filename = "IMG_7959.jpg"
-image = Image.open(filename)
-image = image.convert("L")
-enhancer = ImageEnhance.Contrast(image)
-image = enhancer.enhance(2)
-
-image = image.filter(ImageFilter.MedianFilter())
-image = image.point(lambda p: p > 128 and 255)
-image.show()
-image_arr = np.array(image)
-
-text = pytesseract.image_to_string(image_arr)
-text_arr = text.split("\n")
-
-items = []
-flag1 = False
-flag2 = False
-subtotal = 0
-tax = 0
-total = 0
-
-for i, line in enumerate(text_arr):
-    if "Member" in line:
-        flag1 = True
-    if "SUBTOTAL" in line:
-        flag2 = True
-        subtotal = line.split(" ")[-1]
-    if flag1 and not flag2:
-        line_arr = line.split(" ")
-        if len(line_arr) >= 3:
-            if line_arr[0].isdigit():
-                if "TPD/" in line:
-                    items[-1].append(line)
-                else:
-                    items.append([line])
+pd.set_option("display.max_rows", None)
 
 
-def split_item(item):
-    pattern = r"(\d+)\s(.+?)\s(\d+\.\d{2})"
-    match = re.search(pattern, item)
+def extract_text_from_image(filename: str, show_image: bool = False):
+    image = Image.open(filename)
+    image = image.convert("L")  # convert to greyscale
+    enhancer = ImageEnhance.Contrast(image)
+    image = enhancer.enhance(2)
 
-    if match:
-        # item_id = int(match.group(1))
-        item_name = match.group(2)
-        price = float(match.group(3))
+    image = image.filter(ImageFilter.MedianFilter())
+    if show_image:
+        image.show()
+    image_arr = np.array(image)
 
-        return item_name, price
-    else:
-        # print("No match found.")
-        return None, 0
+    text = pytesseract.image_to_string(image_arr, config="--psm 3")
+
+    data = pytesseract.image_to_data(
+        image_arr, output_type=pytesseract.Output.DICT
+    )
+    print(pd.DataFrame(data).query("conf > 0"))
+    print(text)
+    return text.split("\n")
 
 
-total = 0
-discount = 0
+def process_items_list(text_arr: list) -> tuple:
+    subtotal = 0
 
-print(f"{'Item Name': <20} {'Price': >10}")
-for item in items:
-    item_name, item_price = split_item(item[0])
-    total += item_price
-    print_item = f"{item_name: <20} {item_price: >10.2f}"
+    items = []
+    curr_item = None
+    for i, line in enumerate(text_arr):
+        if "subtotal" in line.lower():
+            subtotal = line.split(" ")[-1]
+            break
+        if line != "":
+            line_arr = line.split(" ")
+            if line_arr[0] == "Saving":
+                items[-1].append(line)
+                continue
+            if "points" in line.lower():
+                continue
+            if not curr_item:
+                curr_item = line
+            else:
+                curr_item += " " + line
+            if utils.is_float(line_arr[-1]):
+                items.append([curr_item])
+                curr_item = None
 
-    if len(item) == 2:
-        _, sale = split_item(item[1])
-        discount += sale
-        print_item += f"\t(-{sale: .2f})"
+    calculated_total = 0
+    for item in items:
+        item_name, item_price = utils.split_item(item[0])
+        if item_price:
+            calculated_total += item_price
 
-    print(print_item)
+    return items, subtotal, calculated_total
 
-print("-------------------")
-print(f"{'Calculated TOTAL': <20} {total: >10.2f}")
-print(f"{'Calculated DISCOUNT': <20} {discount: >10.2f}")
-print(f"{'Calculated SUBTOTAL': <20} {total - discount: >10.2f}")
-print(f"{'Number of items': <20} {len(items): >10}")
-print(f"{'Actual subtotal': <20} {subtotal: >10}")
+
+def jsonify_data(items: list, filename: str, write_to_file: bool = True):
+    items_cleaned = []
+    with open(f"./{filename}_results.csv", "w+") as f:
+        for item in items:
+            item_name, item_price = utils.split_item(item[0])
+            if item_name:
+                items_cleaned.append([item_name, item_price])
+                f.write(item_name + "\n")
+            else:
+                print("**error with item", item[0])
+    return pd.DataFrame(items_cleaned, columns=["name", "price"])
+
+
+def main():
+    filename = "shoppers_1.png"
+    text_arr = extract_text_from_image(filename, show_image=False)
+    items, subtotal, calculated_total = process_items_list(text_arr)
+    data = jsonify_data(items, filename)
+    print(data)
+    # print(data.to_json())
+
+
+if __name__ == "__main__":
+    main()
