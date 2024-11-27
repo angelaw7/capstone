2d65d9377624cc95f1e1d8692bb0dbcd2ad65042
Jason Tan <jasontan62@gmail.com>
2024-11-26 21:53:25 -0500

    adjusting api base url endpoint

--- src/client/api/api.js
+++ src/client/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.2.102:5001/api",
+  baseURL: "http://127.0.0.1:5001/api",
   timeout: 50000,
   headers: {
     "Content-Type": "application/json",
