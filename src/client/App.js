9b0789cf9e68a1e53387ba4427513b4311887ce4
Eric Chen <97481912+chene40@users.noreply.github.com>
2025-01-21 18:55:13 -0500

    feat: add in onboarding page (#165)

--- src/client/App.js
+++ src/client/App.js
@@ -11,6 +11,7 @@ import RegisterView from "./components/view/RegisterView";
 import ResetPasswordView from "./components/view/ResetPasswordView";
 import OverView from "./components/view/OverView";
 import AddExpenseView from "./components/view/AddExpenseView";
+import OnboardingView from "./components/view/OnboardingView";
 import { useFonts } from "expo-font";
 import { TamaguiProvider } from "tamagui";
 import config from "./tamagui.config";
@@ -29,6 +30,7 @@ export default function App() {
           <Stack.Screen name="Login" component={LoginView} />
           <Stack.Screen name="ResetPassword" component={ResetPasswordView} />
           <Stack.Screen name="Register" component={RegisterView} />
+          <Stack.Screen name="Onboarding" component={OnboardingView} />
           <Stack.Screen name="Overview" component={OverView} />
           <Stack.Screen name="MyIncome" component={MyIncome} />
           <Stack.Screen name="NewIncome" component={NewIncomePage} />
