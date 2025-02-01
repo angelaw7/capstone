c626f9a805915b5af22154470d670398ebd1f563
Eric <chene9802@gmail.com>
2025-02-01 16:06:20 -0500

    feat: add in income update/delete endpoints to backend

--- src/server/controllers/incomes/incomes_controller.py
+++ src/server/controllers/incomes/incomes_controller.py
@@ -11,3 +11,10 @@ class IncomesController:
         # Add logic to create income (probs from the image or manual input)
         return IncomesDao.create_income(data.json)
     
+    @staticmethod
+    def update_income(incomeId, updatedData):
+        return IncomesDao.update_income(incomeId, updatedData)
+    
+    @staticmethod
+    def delete_income(incomeId):
+        return IncomesDao.delete_income(incomeId)
--- src/server/daos/incomes/incomes_dao.py
+++ src/server/daos/incomes/incomes_dao.py
@@ -12,23 +12,23 @@ class IncomesDao:
                 "recurring": True,
                 "email": "user@example.com",
                 "created_at": "2025-01-30T14:45:00Z"
-			},
+            },
             {
-				"id": 2,
-				"title": "Part-Time Job",
-				"amount": 1200.00,
-				"recurring": True,
-				"email": "user@example.com",
-				"created_at": "2025-01-25T09:30:00Z"
-			},
+                "id": 2,
+                "title": "Part-Time Job",
+                "amount": 1200.00,
+                "recurring": True,
+                "email": "user@example.com",
+                "created_at": "2025-01-25T09:30:00Z"
+            },
             {
-				"id": 3,
-				"title": "Affiliate Marketing",
-				"amount": 150.00,
-				"recurring": True,
-				"email": "user@example.com",
-				"created_at": "2025-01-28T16:00:00Z"
-			}
+                "id": 3,
+                "title": "Affiliate Marketing",
+                "amount": 150.00,
+                "recurring": True,
+                "email": "user@example.com",
+                "created_at": "2025-01-28T16:00:00Z"
+            }
         ]
 
     @staticmethod
@@ -37,10 +37,44 @@ class IncomesDao:
         # TODO: add expense data to db and adjust return logic 
         return {
             "id": 3, 
-			"title": new_income.title, 
-			"amount": new_income.amount, 
-			"recurring": new_income.recurring, 
-			"frequency": new_income.frequency,
-			"email": new_income.email,
-			"created_at": new_income.created_at,
-		}
+            "title": new_income.title, 
+            "amount": new_income.amount, 
+            "recurring": new_income.recurring, 
+            "frequency": new_income.frequency,
+            "email": new_income.email,
+            "created_at": new_income.created_at,
+        }
+
+    @staticmethod
+    def update_income(incomeId, updatedData):
+        # TODO: implement logic to update income for given user
+        return {
+            "oldIncome": {
+                "id": 1,
+                "title": "Freelance Work",
+                "amount": 500.00,
+                "recurring": True,
+                "email": "user@example.com",
+                "created_at": "2025-01-30T14:45:00Z"
+            },
+            "newIncome" :  {
+                "id": 1,
+                "title": "Freelance Work",
+                "amount": 750.00,
+                "recurring": True,
+                "email": "user@example.com",
+                "created_at": "2025-02-01T14:45:00Z"
+            },
+        }
+    
+    @staticmethod
+    def delete_income(incomeId):
+        # TODO: implement logic to delete a specific income for given user
+        return {
+            "id": 1,
+            "title": "Freelance Work",
+            "amount": 500.00,
+            "recurring": True,
+            "email": "user@example.com",
+            "created_at": "2025-01-30T14:45:00Z"
+        },
