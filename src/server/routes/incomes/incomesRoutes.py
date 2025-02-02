4b3899340225c49d1447852f55cc6c221eb05685
Eric Chen <97481912+chene40@users.noreply.github.com>
2025-02-02 13:38:57 -0500

    feat: add in income update/delete endpoints to backend (#205)

--- src/server/routes/incomes/incomesRoutes.py
+++ src/server/routes/incomes/incomesRoutes.py
@@ -7,8 +7,15 @@ incomes = Blueprint('incomes', __name__)
 def getIncomes(userId):
     return jsonify(IncomesController.get_all_incomes(userId))
 
-
 @incomes.route('/', methods=['POST', 'OPTIONS'])
 def addIncomes():
     new_income = IncomesController.create_income(request)
     return jsonify(new_income), 201
+
+@incomes.route('/<incomeId>', methods=['PUT'])
+def updateIncome(incomeId, updatedData):
+    return jsonify(IncomesController.update_income(incomeId, updatedData))
+
+@incomes.route('/<incomeId>', methods=['DELETE'])
+def deleteIncome(incomeId):
+    return jsonify(IncomesController.delete_income(incomeId))
