43d3c9faa46407c09b36b0440f87d7bc5b296800
Eric <chene9802@gmail.com>
2025-03-09 18:08:16 -0400

    test: api test for income endpoint

--- src/server/tests/income/test_income.py
+++ src/server/tests/income/test_income.py
@@ -8,19 +8,30 @@ from app import app
 @pytest.fixture
 def client():
     with app.test_client() as client:
+        app.testing = True
+
+		# Mock data
+        app.incomes = {
+			"title": "Test Data",
+			"amount" : 100,
+			"recurring": False,
+			"frequency": None,
+			"email" : "hi@email.com"
+		}
         yield client
         
-
+""" GET """
 def test_get_incomes_valid_user(client):
-	test_email = "hi@email.com"
+	mock_email = app.incomes["email"]
 
-	response = client.get(f'/api/incomes/{test_email}')
+	response = client.get(f'/api/incomes/{mock_email}')
 	assert response.status_code == 200
      
 	income_list = response.get_json()
 	for income in income_list:
-		assert income['email'] == test_email
+		assert income['email'] == mock_email
 	
+""" GET """
 def test_get_incomes_invalid_user(client):
 	test_email = "nonexistentuser@test.com"
 
@@ -28,3 +39,37 @@ def test_get_incomes_invalid_user(client):
 	""" To be implemented """
 	# assert response.status_code == 404
 
+""" POST """
+def test_create_income(client):
+	mock_data = app.incomes
+	# Cleanup by test_delete_income()
+	response = client.post('/api/incomes/', json=mock_data)
+	print(response.status_code)
+	assert response.status_code == 201
+
+
+""" PUT """
+def test_update_income(client):
+	res = client.get(f'/api/incomes/{app.incomes["email"]}').get_json()
+	id = res[len(res)-1]["id"]
+
+	mock_data = res[len(res)-1]
+	mock_data["amount"] += 100
+
+	response = client.put(f'/api/incomes/{id}', json=mock_data)
+	assert response.status_code == 200
+
+	data = response.get_json()[0]
+	assert data['id'] == id
+
+
+""" DELETE """
+def test_delete_income(client):
+	res = client.get(f'/api/incomes/{app.incomes["email"]}').get_json()
+	id = res[len(res)-1]["id"]
+
+	response = client.delete(f'/api/incomes/{id}')
+	assert response.status_code == 200
+
+	data = response.get_json()[0]
+	assert data['id'] == id
