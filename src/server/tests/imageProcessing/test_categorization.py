7a9a40ece7414f3ad0724b5aba2309f565317027
Angela Wang <wangela472@gmail.com>
2025-03-10 12:19:28 -0400

    tests: setup for testing categorization

--- src/server/tests/imageProcessing/test_categorization.py
+++ src/server/tests/imageProcessing/test_categorization.py
@@ -7,3 +7,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from imageProcessing import categorization
 
 categorizer = categorization.Categorizer()
+
+def test_categorization():
+    
