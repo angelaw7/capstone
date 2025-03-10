47b4506ffa86cfaed573420c29abae76b4a01897
Angela Wang <wangela472@gmail.com>
2025-03-10 16:51:22 -0400

    tests: move parsing stuff to folder, update links

--- src/server/tests/imageProcessing/test_categorization.py
+++ src/server/tests/imageProcessing/test_categorization.py
@@ -1,12 +1,17 @@
 import pytest
 import sys
 import os
+import pandas as pd
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
-from imageProcessing import categorization
+from server.imageProcessing.categorization import Categorizer
 
-categorizer = categorization.Categorizer()
+categorizer = Categorizer()
 
 def test_categorization():
-    pass
+    with open("./data/categorization/receipt_items_input.csv", "r") as f:
+        items = pd.read_csv(f)
+    output = categorizer.categorize(items=items)
+    with open("./data/categorization/receipt_items_output.csv", "w") as f:
+        f.write(output.to_csv(index=False))
--- src/server/tests/imageProcessing/test_parsing.py
+++ src/server/tests/imageProcessing/test_parsing.py
@@ -6,19 +6,20 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 from server.imageProcessing.process import ImageProcessor
 
 processor = ImageProcessor()
+IMAGE_PATH = "./data/parsing"
 
-def _test_parse_image(receipt_filename: str):
-    output_filename = receipt_filename + ".output.csv"
-    receipt, data = processor._parse_image(receipt_filename)
+
+def _test_parse_image(receipt_filepath: str, filename: str):
+    output_filename = os.path.join(IMAGE_PATH, "output", filename + ".output.csv")
+    receipt, data = processor._parse_image(receipt_filepath)
     data = data[["name", "price"]]
     with open(output_filename, "w") as f:
         f.write(data.to_csv())
 
 
 def test_parse_images():
-    image_path = "./data/parsing"
+    image_path = os.path.join(IMAGE_PATH, "input")
     files = os.listdir(image_path)
     for file in files:
-        if not file.endswith(".csv"):
-            _test_parse_image(os.path.join(image_path, file))
+        _test_parse_image(os.path.join(image_path, file), file)
 
