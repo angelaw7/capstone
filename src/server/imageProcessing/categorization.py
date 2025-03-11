b8d28d384519cec99cf86015753a5045464d3561
Angela Wang <74735037+angelaw7@users.noreply.github.com>
2025-03-10 21:00:43 -0400

    docs: add model/ocr accuracy to vnv report (#255)

--- src/server/imageProcessing/categorization.py
+++ src/server/imageProcessing/categorization.py
@@ -1,8 +1,16 @@
+from dotenv import load_dotenv
 import pandas as pd
 import pickle
-
 import os
 
+import cohere
+
+
+load_dotenv()
+
+
+COHERE_API_KEY = os.environ.get("cohere_api_key")
+COHERE_MODEL_ID = os.environ.get("cohere_model_id")
 
 class Categorizer:
     def __init__(self,
@@ -20,11 +28,21 @@ class Categorizer:
 
         with open(os.path.join(base_path, label_encoder_path), "rb") as f:
             self.label_encoder = pickle.load(f)
+        self.co = cohere.Client(COHERE_API_KEY)
+        self.model_id = COHERE_MODEL_ID
 
     def categorize(self, items: pd.DataFrame) -> pd.DataFrame:
-        items_transformed = self.vectorizer.transform(items["name"])
-        y_pred = self.model.predict(items_transformed)
-        predicted_categories = self.label_encoder.inverse_transform(y_pred)
-
-        items["category"] = predicted_categories
+        response = self.co.classify(
+            model=self.model_id,
+            inputs=items["name"].tolist())
+        predictions = response.classifications
+        categories = [prediction.prediction for prediction in predictions]
+        items["category"] = categories
         return items
+
+        # items_transformed = self.vectorizer.transform(items["name"])
+        # y_pred = self.model.predict(items_transformed)
+        # predicted_categories = self.label_encoder.inverse_transform(y_pred)
+
+        # items["category"] = predicted_categories
+        # return items
